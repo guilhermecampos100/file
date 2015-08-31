faf0795d510e52d360112ef08edc42d865ee98c0
guilhermecampos100 <guilhermecampos100@gmail.com>
2015-08-31 13:53:22 -0300

    inseridas funcoes de acao para todos os botoes

--- js/index.js
+++ js/index.js
@@ -16,6 +16,17 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ 
+ //generic getById
+function getById(id) {
+return document.querySelector(id);
+}
+//generic content logger
+function logit(s) {
+getById("#content").innerHTML += s;
+}
+
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -58,8 +69,88 @@ var app = {
 		alert('sucesso');
 		fileSystem = fs;
 		alert('sucesso 2');
+		
+		getById("#dirListingButton").addEventListener("touchstart",doDirectoryListing);
+		getById("#addFileButton").addEventListener("touchstart",doAppendFile);
+		getById("#readFileButton").addEventListener("touchstart",doReadFile);
+		getById("#metadataFileButton").addEventListener("touchstart",doMetadataFile);
+		getById("#deleteFileButton").addEventListener("touchstart",doDeleteFile);
+
+		logit( "Got the file system: "+fileSystem.name +"<br/>" + "root entry name is "+fileSystem.root.name + "<p/>")
+
+		doDirectoryListing();
+		
+		
 	},
 	onError: function() {
 		alert('deu erro');
 	}	
 };
+
+function doDeleteFile(e) {
+fileSystem.root.getFile("test.txt", {create:true}, function(f) {
+f.remove(function() {
+logit("File removed<p/>");
+});
+}, onError);
+}
+
+function metadataFile(m) {
+logit("File was last modified "+m.modificationTime+"<p/>");
+}
+
+function doMetadataFile(e) {
+fileSystem.root.getFile("test.txt", {create:true}, function(f) {
+f.getMetadata(metadataFile,onError);
+}, onError);
+}
+
+function readFile(f) {
+reader = new FileReader();
+reader.onloadend = function(e) {
+console.log("go to end");
+logit("<pre>" + e.target.result + "</pre><p/>");
+}
+reader.readAsText(f);
+}
+
+function doReadFile(e) {
+fileSystem.root.getFile("test.txt", {create:true}, readFile, onError);
+}
+
+function appendFile(f) {
+
+f.createWriter(function(writerOb) {
+writerOb.onwrite=function() {
+logit("Done writing to file.<p/>");
+}
+//go to the end of the file...
+writerOb.seek(writerOb.length);
+writerOb.write("Test at "+new Date().toString() + "\n");
+})
+
+}
+
+function doAppendFile(e) {
+fileSystem.root.getFile("test.txt", {create:true}, appendFile, onError);
+}
+
+function gotFiles(entries) {
+var s = "";
+for(var i=0,len=entries.length; i<len; i++) {
+//entry objects include: isFile, isDirectory, name, fullPath
+s+= entries[i].fullPath;
+if (entries[i].isFile) {
+s += " [F]";
+}
+else {
+s += " [D]";
+}
+s += "<br/>";
+
+}
+s+="<p/>";
+logit(s);
+}
+
+
